f8e217e1af6e2d631d4c60471faeb408d6e1bede
CorneTubage <150783232+CorneTubage@users.noreply.github.com>
2025-11-17 16:02:46 +0000

    extension vuejs

--- apps/helloworld/lib/Controller/ApiController.php
+++ apps/helloworld/lib/Controller/ApiController.php
@@ -4,28 +4,40 @@ declare(strict_types=1);
 
 namespace OCA\HelloWorld\Controller;
 
-use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\Attribute\ApiRoute;
 use OCP\AppFramework\Http\Attribute\NoAdminRequired;
+use OCP\AppFramework\Http\Attribute\NoCSRFRequired;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\OCSController;
+use OCP\IRequest;
+use OCP\IUserManager;
 
-/**
- * @psalm-suppress UnusedClass
- */
 class ApiController extends OCSController {
-	/**
-	 * An example API endpoint
-	 *
-	 * @return DataResponse<Http::STATUS_OK, array{message: string}, array{}>
-	 *
-	 * 200: Data returned
-	 */
-	#[NoAdminRequired]
-	#[ApiRoute(verb: 'GET', url: '/api')]
-	public function index(): DataResponse {
-		return new DataResponse(
-			['message' => 'Hello world!']
-		);
-	}
+    private IUserManager $userManager;
+
+    public function __construct(string $AppName, IRequest $request, IUserManager $userManager) {
+        parent::__construct($AppName, $request);
+        $this->userManager = $userManager;
+    }
+
+    #[NoAdminRequired]
+    #[NoCSRFRequired]
+    #[ApiRoute(verb: 'GET', url: '/api')]
+    public function index(): DataResponse {
+        return new DataResponse(['message' => 'Hello world!']);
+    }
+
+    #[NoAdminRequired]
+    #[NoCSRFRequired]
+    #[ApiRoute(verb: 'GET', url: '/api/users')]
+    public function getUsers(): DataResponse {
+        $users = [];
+        foreach ($this->userManager->search('') as $user) {
+            $users[] = [
+                'uid' => $user->getUID(),
+                'displayName' => $user->getDisplayName(),
+            ];
+        }
+        return new DataResponse($users);
+    }
 }
--- apps/helloworld/src/App.vue
+++ apps/helloworld/src/App.vue
@@ -1,26 +1,79 @@
 <template>
-	<NcAppContent>
-		<div id="helloworld">
-			<h1>Hello world!</h1>
-		</div>
-	</NcAppContent>
+  <div class="user-list">
+    <h2>Utilisateurs Nextcloud</h2>
+    <button @click="fetchUsers">Afficher les utilisateurs</button>
+
+    <ul v-if="users.length">
+      <li v-for="user in users" :key="user.uid">
+        {{ user.displayName }} ({{ user.uid }})
+      </li>
+    </ul>
+
+    <p v-else>Aucun utilisateur chargé</p>
+  </div>
 </template>
 
 <script>
-import NcAppContent from '@nextcloud/vue/dist/Components/NcAppContent.js'
-
 export default {
-	name: 'App',
-	components: {
-		NcAppContent,
-	},
+  name: 'UserList',
+  data() {
+    return {
+      users: [],
+    }
+  },
+  methods: {
+    async fetchUsers() {
+      try {
+        const response = await fetch(
+          OC.generateUrl('/ocs/v2.php/apps/helloworld/api/users'),
+          {
+            headers: {
+              'OCS-APIREQUEST': 'true'
+            }
+          }
+        )
+        const json = await response.json()
+        this.users = json.ocs.data
+      } catch (e) {
+        console.error('Erreur lors du chargement des utilisateurs', e)
+      }
+    }
+  }
 }
 </script>
 
-<style scoped lang="scss">
-#helloworld {
-	display: flex;
-	justify-content: center;
-	margin: 16px;
+<style scoped>
+.user-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+}
+
+button {
+  background-color: #0082c9;
+  color: white;
+  border: none;
+  border-radius: 6px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #006ba1;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin-top: 20px;
+}
+
+li {
+  margin: 6px 0;
+  background: #f4f4f4;
+  padding: 8px;
+  border-radius: 4px;
 }
 </style>

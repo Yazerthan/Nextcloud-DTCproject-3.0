d40946091e494e8eda00de660b95a98f13e79e90
CorneTubage <150783232+CorneTubage@users.noreply.github.com>
2025-11-25 15:45:01 +0000

    App.vue Helloword update

--- apps/helloworld/src/App.vue
+++ apps/helloworld/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="user-list">
     <h2>Utilisateurs Nextcloud</h2>
-    <button @click="fetchUsers">Afficher les utilisateurs</button>
 
-    <ul v-if="users.length">
-      <li v-for="user in users" :key="user.uid">
-        {{ user.displayName }} ({{ user.uid }})
-      </li>
-    </ul>
+    <div class="container" v-if="showUsers">
+      <p class="userText">Nos utilisateurs</p>
+      <div class="user__container">
+        <button class="user_button" v-for="user in users" :key="user.uid">
+          {{ user.displayName }} ({{ user.uid }})
+        </button>
+      </div>
+    </div>
 
-    <p v-else>Aucun utilisateur chargé</p>
+    <button class="showUser" @click="fetchUsers">
+      {{ showUsers ? 'Cacher les utilisateurs' : 'Afficher les utilisateurs' }}
+    </button>
   </div>
 </template>
+
 
 <script>
 export default {
@@ -19,26 +24,50 @@ export default {
   data() {
     return {
       users: [],
+      showUsers: false,
+      loaded: false,
+      showUserName: false,
     }
   },
   methods: {
     async fetchUsers() {
+      if (this.loaded) {
+        this.showUsers = !this.showUsers
+        return
+      }
+
       try {
         const response = await fetch(
-          OC.generateUrl('/ocs/v2.php/apps/helloworld/api/users'),
+          '/ocs/v2.php/apps/helloworld/api/users?format=json',
           {
             headers: {
-              'OCS-APIREQUEST': 'true'
+              'OCS-APIREQUEST': 'true',
+              'Accept': 'application/json'
             }
           }
         )
+
         const json = await response.json()
+
         this.users = json.ocs.data
+        this.loaded = true 
+        this.showUsers = true
+
       } catch (e) {
         console.error('Erreur lors du chargement des utilisateurs', e)
       }
     }
-  }
+  },
+  // methods: {
+  //   async UserName(){
+  //     if (!showUserName) {
+  //       showUserName = true;
+  //     } else {
+  //       showUserName = false;
+  //     }
+  //   }
+
+  // }
 }
 </script>
 
@@ -51,29 +80,47 @@ export default {
   width: 100vw;
 }
 
-button {
+h2 {
+  margin-bottom: 6px;
+}
+
+.container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  margin: 20px 0;
+}
+
+.userText {
+  font-size: x-large;
+}
+
+.user__container {
+  display: flex;
+  flex-direction: row;
+  gap: 1.5rem;
+}
+
+.user_button {
+  background: #fff;
+  color: #00679e;
+  border-radius: 8px;
+  border: 1px solid #00679e;
+}
+
+.showUser {
   background-color: #0082c9;
   color: white;
   border: none;
   border-radius: 6px;
   padding: 10px 20px;
   cursor: pointer;
+  margin: 6px 0 0 !important;
 }
 
-button:hover {
+.showUser:hover {
   background-color: #006ba1;
 }
 
-ul {
-  list-style: none;
-  padding: 0;
-  margin-top: 20px;
-}
-
-li {
-  margin: 6px 0;
-  background: #f4f4f4;
-  padding: 8px;
-  border-radius: 4px;
-}
 </style>
